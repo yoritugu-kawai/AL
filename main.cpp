355eac279dc2fefe632b40af13cd5f0a06d30de8
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-04-19 15:29:42 +0900

    01_-01

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
 	win->CreateGameWindow(L"LE2B_08_カワイ_ヨリツグ_AL3");
-
+	 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
