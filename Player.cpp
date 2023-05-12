bf0622664444df1f00b2f09ce81f7e8391a90bb8
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-05-12 15:09:11 +0900

    t

--- Player.cpp
+++ Player.cpp
@@ -1,24 +1,18 @@
-#include"Player.h"
+#include "Player.h"
 #include <cassert>
-void Player::Initialize(Model* model, uint32_t textureHandle) { 
+void Player::Initialize(Model* model, uint32_t textureHandle) {
 	assert(model);
 	this->model_ = model;
 	this->textureHandle_ = textureHandle;
 
-		worldTransform_.Initialize();
-
-
-
-
+	worldTransform_.Initialize();
 }
 
 void Player::Update() { 
-	worldTransform_.TransferMatrix();
-
+	worldTransform_.TransferMatrix(); 
 }
 
 void Player::Draw(ViewProjection viewProjection_) {
 
-model_->Draw(this->worldTransform_, viewProjection_, this->textureHandle_);
-
+	model_->Draw(this->worldTransform_, viewProjection_, this->textureHandle_);
 }
