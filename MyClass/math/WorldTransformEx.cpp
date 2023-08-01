97d8d6f2e39145045f9b32797fdafd23372a9fc0
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-08-01 15:17:50 +0900

    kk

--- MyClass/math/WorldTransformEx.cpp
+++ MyClass/math/WorldTransformEx.cpp
@@ -5,5 +5,8 @@
 #include <cassert>
 void WorldTransform::UpdateMatrix() {
 	matWorld_ = MakeAffineMatrix(scale_, rotation_, translation_);
+	if (parent_) {
+		matWorld_ *= parent_->matWorld_;
+	}
 	TransferMatrix();
 }
