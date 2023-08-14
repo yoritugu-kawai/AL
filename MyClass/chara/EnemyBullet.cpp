e0db2aad212f7ad0e76416e15eb50928f0e4dd2f
k022g0056@g.neec.ac.jp <k022g0056@g.neec.ac.jp>
2023-08-14 22:55:14 +0900

    dd

--- MyClass/chara/EnemyBullet.cpp
+++ MyClass/chara/EnemyBullet.cpp
@@ -1,11 +1,24 @@
-#include "EnemyBullet.h"
+﻿#include "EnemyBullet.h"
 
 void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector3& velocuty) {
 
 	model_ = model;
-
+	//長さ
+	worldTransform_.scale_.x = 0.5f;
+	worldTransform_.scale_.y = 0.5f;
+	worldTransform_.scale_.z = 3.0f;
+	//
+	
 	textureHandle_ = TextureManager::Load("bullet2.png");
 	worldTransform_.Initialize();
+	
+	//
+	float velocityXZ = sqrt(velocuty.x * velocuty.x + velocuty.z * velocuty.z);
+
+	float heightY = -velocuty.y;
+	worldTransform_.rotation_.y = std::atan2(velocuty.x, velocuty.z);
+	worldTransform_.rotation_.x = std::atan2(heightY, velocityXZ);
+
 
 	worldTransform_.translation_ = position;
 
